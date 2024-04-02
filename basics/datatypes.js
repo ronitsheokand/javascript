821e89686e19080e0782763c22931a83bf4b8ad7
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2024-04-02 19:39:21 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict";//use newest version of javascript
+
+//alert(3+3)// its shows popup but in browser ,we are using node js
+
+//code redability
+console.log(3+3)
+console.log("sum")
+
+let name="ronit"
+let age=20
+let idLoggedIn=true
+let state=null
+
+//number = 2 to power 53
+//n=bigint
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined
+//symbol = unique
+
+
+console.log(typeof "ronit")
+console.log(typeof undefined)
+console.log(typeof null)
+
+

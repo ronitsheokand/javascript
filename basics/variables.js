e1712b19c4c8a6741b94bc0f3cb8d4ee75cdefa5
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2024-04-02 19:25:33 +0000

    c

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=1234
+let accountEmail="abc@gmail.com" 
+var accountPassword="82837dhfh" //do not use var instead use let
+accountNumber=12
+
+//we can not chage value of const,, others we can changelike
+accountPassword="1234"
+
+console.log(accountId)
+
+console.table([accountEmail,accountPassword,accountNumber])

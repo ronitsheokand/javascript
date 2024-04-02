48cfb9817b3440cda1c47868cf46e1412bf26c18
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2024-04-02 19:59:28 +0000

    conversion of datatypes

---
+++ basics/conversionoperation.js
@@ -0,0 +1,44 @@
+/*let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+
+let isLoggedIn = "ronit"
+
+let booleanIsLoggesIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggesIn);
+
+
+//"" = = false
+//1 = true ; 0 = false
+//"ronit" = true
+
+let number=33
+
+let stringNumber= String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+

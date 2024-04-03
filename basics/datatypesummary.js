d9dd9d74a7e046dbbb2451fb08c9fa3000a4b283
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2024-04-03 02:03:07 +0000

    function

---
+++ basics/datatypesummary.js
@@ -0,0 +1,53 @@
+// primitive
+
+//7 types : String ,Number , Boolean, null ,undefined , Symbol , BigInt
+
+const score = 100
+const pointValue = 100.3
+
+const loggedIn =false
+const temp= null
+let email;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// symbol ka kaam hi yhi hai same value doge to bhi equal nhi hogi
+console.log(id === anotherId );
+
+
+ //const BigNumber = 982420873n  this is bigInt
+
+
+
+
+// reference (non- primitive)
+
+//Array , Objects , Functions
+
+
+const arr1=["ronit","sheokand","ok"];
+let myObj={
+    name:"name",
+    age:20
+}
+//console.log(arr1)
+//console.log(myObj)
+
+
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+
+console.log(typeof BigNumber);
+console.log(typeof myFunction);
+
+
+
+
+
+
+
+

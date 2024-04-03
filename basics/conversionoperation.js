d4c768fa7816bad52769824f9df5d8cf33744815
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2024-04-03 01:21:01 +0000

    operations

--- basics/conversionoperation.js
+++ basics/conversionoperation.js
@@ -23,7 +23,7 @@ console.log(typeof (score));
 
 let valueInNumber=Number(score)
 console.log(typeof valueInNumber);
-console.log(valueInNumber);*/
+console.log(valueInNumber);
 
 
 let isLoggedIn = "ronit"
@@ -40,5 +40,40 @@ let number=33
 
 let stringNumber= String(number)
 console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);*/
 
+
+// *********  operations  **************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**5);
+//console.log(2/2);
+//console.log(2%2);
+
+
+let str1 ="ronit ";
+let str2 = "sheokand";
+
+let str3 = str1+str2
+/*console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");*/
+
+//console.log(+true);
+//console.log(+"");
+
+let plus = 100
+++plus;
+plus++;
+console.log(plus);

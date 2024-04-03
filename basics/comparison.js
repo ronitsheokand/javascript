ab1c7792e25b9dfa40bbccee8a3e4b59b26766b8
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2024-04-03 01:32:34 +0000

    comparison

---
+++ basics/comparison.js
@@ -0,0 +1,17 @@
+/*console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);*/
+
+//console.log("2" > 1);
+
+//console.log(null > 0); false
+//console.log(null == 0); false
+//console.log(null >= 0); true
+ //because comparison convert null to an number 0
+ // comaparing with undefined will give you all three false
+
+
+ console.log("2" === 2);
+// tripe equal also check datatype k

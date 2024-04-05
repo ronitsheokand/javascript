2101fcd7520bc35341bbae9e9bf614fde1356db8
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2024-04-05 09:09:28 +0000

    number and math

---
+++ basics/numbersAndMath.js
@@ -0,0 +1,45 @@
+const number = 100
+
+const balance = new Number(300) //number hi rhega 300
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(4))  // decimal ke baad digit kitne honge
+
+
+const otherNumber = 23.4847
+
+//console.log(otherNumber.toPrecision(2)); // kitne digit chahiye
+
+
+const hundred = 1000000
+//console.log(hundred.toLocaleString); // comas laga dege 
+
+
+
+//++++++++++++++++++++++++    maths   ++++++++++++++++++++
+
+
+
+console.log(Math.abs(-5));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6)); // isse bada (5) output ayga
+console.log(Math.floor(4.6)); // isse chota(4) output
+
+
+
+console.log(Math.random()); // value 0 or 1 ke bich mai random ayegi
+console.log(Math.random()*10); // value 0 se 10 ke bich mai
+console.log((Math.random()*10)+1); // value 1 or 10 ke bich
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min))+min);
+
+
+
+
+
+

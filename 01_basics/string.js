a2c913850f3c7bc694a099766e77533de73fc787
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2024-04-09 19:11:11 +0000

    changes

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -5,7 +5,7 @@ const age = 20
 
 const stringName = new String('ronit-sheokand')
 
-//console.log(stringName[0]);
+//console.log(stringName[3]);
 //console.log(stringName.__proto__); // to see protoype
 
 
@@ -18,24 +18,24 @@ const stringName = new String('ronit-sheokand')
 
 
 const string1 =  stringName.substring(2,9)
-console.log(string1);
+// console.log(string1);
 
 const string2 = stringName.slice(-9,11)
-console.log(string2);   // index pe se value uthayega
+// console.log(string2);   // index pe se value uthayega
 
 const string3 = "     abcd     "
-console.log(string3.trim());
-console.log(string3); // spaces cut krdega
+// console.log(string3.trim());
+// console.log(string3); // spaces cut krdega
 
 const string4 = "abcdefghikjlmnopqrstuvwxyz"
-console.log(string4.replace('lmnop', 'AAAA')); // word replace krdega
+//console.log(string4.replace('lmnop', 'AAAA')); // word replace krdega
 
 
 const string5 = "abcdefghijklmnopqrstuvwxyz"
-console.log(string5.includes('abcd'));  //word included hai ya nhi
+//console.log(string5.includes('abcd'));  //word included hai ya nhi
 
 const string6 = stringName
-console.log(string6.split('n'));  // jo bhi cheez doge us jagah se array mai split krdega
+//console.log(string6.split('n'));  // jo bhi cheez doge us jagah se array mai split krdega
 
 
 

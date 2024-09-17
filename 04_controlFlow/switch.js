a5f62b8795966c7503bb3bfbcc2b3965c7e698ad
ronit7202 <128242677+ronitsheokand@users.noreply.github.com>
2024-09-17 14:39:12 +0000

    26

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,19 @@
+const month="march"
+
+switch(month){
+    case "jan":
+        console.log("january");
+        break;
+
+        case "feb":
+            console.log("february");
+            break;
+
+           case "march":
+            console.log("march");
+            break;
+
+            default:
+                console.log("default");
+                break;   
+}
